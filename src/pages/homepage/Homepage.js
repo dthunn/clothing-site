c36464f3880738e0c9282d068a8f0ab5c105dc53
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2020-02-17 12:57:04 -0600

    after section 10

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import Directory from "../../components/directory/Directory";
+import Directory from '../../components/directory/Directory';
 
-import "./homepage.styles.scss";
+import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
